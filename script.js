694cfc5a6f55829bf2212e92dec560e810a99c66
maryamgh1988 <36102515+maryamgh1988@users.noreply.github.com>
2021-03-25 10:19:57 +0000

    add newtas function

---
+++ script.js
@@ -0,0 +1,22 @@
+const newtask= document.querySelector("#addList").value;
+const addnewtask= document.querySelector("#submit");
+function addNewTask(){
+    let div = document.querySelector('.listItemsContainer');
+    let newli = document.createElement('li');
+    div.appendChild(newli);     
+    newli.classList.add('listItem');
+newli.insertAdjacentHTML('beforeend',  ` <input type="checkbox" class="checkbox"></input>
+<p class="task">${newtask}</p>
+<i class="fa fa-trash-o" ></i>`);
+
+
+    // newli.innerText = ` <input type="checkbox" class="checkbox"></input>
+    // <p class="task">fjhfjdhjkf</p>
+    // <i class="fa fa-trash-o" ></i>`;
+  
+   
+}
+addnewtask.addEventListener("click", (event) => {
+    event.preventDefault();
+    addNewTask();
+});
