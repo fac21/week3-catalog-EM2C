744c4f20f4823cb9d08097b285f47fdbcddfca7b
maryamgh1988 <36102515+maryamgh1988@users.noreply.github.com>
2021-03-25 12:38:22 +0000

    delet function

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 const addnewtask = document.querySelector("#submit");
 let liID = 0;
+let liClass =0;
 function addNewTask() {
     let newtask = document.querySelector("#addList").value;
     if (newtask) {
@@ -10,21 +11,41 @@ function addNewTask() {
         let newli = document.createElement('li');
         listContainer.appendChild(newli);
         newli.classList.add('listItem');
-        newli.setAttribute("id", liID);
+        newli.setAttribute("id", `li${liID}`);
         console.log(newli)
 
         console.log(liID)
-        newli.insertAdjacentHTML('beforeend', ` <input type="checkbox" class="checkbox"></input>
-    <p class="task">${newtask}</p>
-    <i id="trash${liID}" class="fa fa-trash-o" ></i>`);
+      
+      newli.insertAdjacentHTML('beforeend', ` <input id="input${liID}" type="checkbox" class="checkbox"></input>
+      <label for="input${liID}">${newtask}</label>
+      
+    <i class=" trash${liClass} fa fa-trash-o" ></i>`);
         document.querySelector("#addList").value = "";
-        liID++
+        liID++;
+        liClass++;
         console.log(liID)
+
+
+
 
     } else {
         alert('fill the form')
     }
 
+/*********************************** */
+
+const delTask = document.querySelector(`.trash0`);
+delTask.addEventListener("click", (event) => {
+   
+    console.log('mmm');
+    console.log(event.target)
+    event.preventDefault();
+    deleteTask();
+});
+function deleteTask() {
+    console.log('kdk')
+     document.querySelector('#li0').remove()
+}
 
 }
 addnewtask.addEventListener("click", (event) => {
@@ -35,15 +56,17 @@ addnewtask.addEventListener("click", (event) => {
 
 
 /**************************************DELET TASK */
-// const delTask = document.querySelector('#trash0');
+// const delTask = document.querySelector(`#trash${liID}`);
 // delTask.addEventListener("click", (event) => {
-//     console.log('mmm')
+   
+//     console.log('mmm');
+//     console.log(event.target)
 //     event.preventDefault();
 //     deleteTask();
 // });
 // function deleteTask() {
-//     console.log('mmm')
-//     document.querySelector('#0').remove()
+//     console.log('kdk')
+//     // document.querySelector('#lifirst').remove()
 // }
 
 
